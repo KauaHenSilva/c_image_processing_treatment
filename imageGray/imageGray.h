6ab0d51aa5048d4f74825fe77a0dee29de8f274f
KauaHenSilva <kaua.sbc@gmail.com>
2024-05-10 21:50:04 -0300

    Update: ModularCrust

--- imageGray/imageGray.h
+++ imageGray/imageGray.h
@@ -1,7 +1,7 @@
 typedef struct pixel PixelRGB;
 typedef struct image Image;
 
-//nova
+// nova
 typedef struct pixelgray Pixelgray;
 typedef struct imageGray ImageGray;
 /*
@@ -9,13 +9,13 @@ Novas funcoes
 - ler txt e converter em imagem -> Image
 - exportar imagem para txt -> txt salvo
 - Converter imagem RGB para nivel de cinza, isto é, com um único valor de pixel -> ImageGray
-- aplicar clusterizacao na imagem a partir de um arquivo txt com sementes, 
+- aplicar clusterizacao na imagem a partir de um arquivo txt com sementes,
 	adtaptar a funcao da distancia euclidiana para considerar apenas os valores do pixel -> salvar txt com o resultado da clusterizacao
 */
 
-ImageGray* converterParaCinza(Image *imagemrgb);
+ImageGray *converterParaCinza(Image *imagemrgb);
 void printPixelGray(int x, int y, ImageGray *img);
 void printPixelGray(int x, int y, ImageGray *img);
 void printImageGrey(ImageGray *img);
-
-
+void printImagemCrusterizada(ImageGray *imgCrust);
+ImageGray *crusterizacao(ImageGray *imgGray);
--- main.c
+++ main.c
@@ -9,55 +9,6 @@
 #include <unistd.h>
 #endif
 
-ImageGray *crusterizacao(ImageGray *imgGray)
-{
-  FILE *seed = fopen("/home/user/dever_oseas/seed.txt", "r");
-  if (!seed)
-#ifndef _WIN32
-    perror("Não leu");
-#else
-    exit(EXIT_FAILURE);
-#endif
-
-  // X   Y   DIFERENCA
-  // 310 290 30
-  // 121 65 20
-  // 199 393 20
-  // 218 91 30
-  // 287 159 30
-
-  ImageGray *imgCrust = (ImageGray *)malloc(sizeof(ImageGray));
-  imgCrust->altura = imgGray->altura;
-  imgCrust->largura = imgGray->largura;
-  imgCrust->pixel = (Pixelgray *)malloc(imgCrust->altura * imgCrust->largura * sizeof(Pixelgray));
-
-  int x, y, diferenca, pixelAtual;
-  int resultado;
-  while (fscanf(seed, "%d %d %d %d", &x, &y, &diferenca, &resultado) != EOF)
-  {
-    int pCinza = imgGray->pixel[x * larguraGlob + y].r;
-    for (int i = 0; i < imgGray->altura; i++)
-      for (int j = 0; j < imgGray->largura; j++)
-      {
-        pixelAtual = imgGray->pixel[i * larguraGlob + j].r;
-        
-        int diferencaAtual = pCinza - pixelAtual;
-        if (diferencaAtual < 0)
-          diferencaAtual *= -1;
-
-        if (diferencaAtual <= diferenca)
-        {
-          imgCrust->pixel[i * larguraGlob + j].r = resultado;
-          imgCrust->pixel[i * larguraGlob + j].g = resultado;
-          imgCrust->pixel[i * larguraGlob + j].b = resultado;
-        }
-      }
-  }
-
-  fclose(seed);
-  return imgCrust;
-}
-
 int main()
 {
   fileRGB = fopen("/home/user/dever_oseas/input_image.txt", "r");
@@ -77,18 +28,12 @@ int main()
   // printImage(img);
 
   ImageGray *imgGray = converterParaCinza(img);
-  ImageGray *imgCrust = crusterizacao(imgGray);
-
-  for (int i = 0; i < imgCrust->altura; i++)
-  {
-    for (int j = 0; j < imgCrust->largura; j++)
-    {
-      printf("%d", imgCrust->pixel[i * larguraGlob + j].r);
-    }
-    printf("\n");
-  }
 
   // printImageGrey(imgGray);
+
+  ImageGray *imgCrust = crusterizacao(imgGray);
+
+  printImagemCrusterizada(imgCrust);
 
   liberacaodeImage(img);
   liberacaodeImageGray(imgCrust);
